c1d82da10b7e9788c3394d450f4c1bc740da44a4
Netotest <athomson@mail.ru>
2024-04-24 21:50:11 +0300

    initial commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int s  = 1000; // Счет на телефоне
+        int s = 1000; // Счет на телефоне
         System.out.println("Счет на телефоне до пополнения " + s);
         int sp = 5000; // Cумма пополнения
         System.out.println("Cумма пополнения " + sp);
-        if ( sp >= 1000 ){
+        if (sp >= 1000) {
             float b = (float) (sp * 0.01); // Сумма бонусов
-            System.out.println( "Сумма бонусов при пополнении " + b);
+            System.out.println("Сумма бонусов при пополнении " + b);
             int fs = (int) (s + sp + b); // Сумма на счете, вместе с бонусами
             System.out.println("Итоговая сумма на счете с бонусами " + fs);
         } else {
             float b = (float) (sp * 0); // Сумма бонусов
-            System.out.println( "Сумма бонусов при пополнении " + b);
+            System.out.println("Сумма бонусов при пополнении " + b);
             int fs = (int) (s + sp + b); // Сумма на счете, вместе с бонусами
             System.out.println("Итоговая сумма на счете с бонусами " + fs);
         }
-
-
-
 
 
         // Объявляете переменные для входных данных и
