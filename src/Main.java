4c4d7a00006e8f6ec1cabe3d6972e270b75abe5a
alexthomson031 <166239780+alexthomson031@users.noreply.github.com>
2024-04-27 10:27:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int s = 1000; // Счет на телефоне
-        System.out.println("Счет на телефоне до пополнения " + s);
-        int sp = 5000; // Cумма пополнения
-        System.out.println("Cумма пополнения " + sp);
-        if (sp >= 1000) {
-            float b = (float) (sp * 0.01); // Сумма бонусов
-            System.out.println("Сумма бонусов при пополнении " + b);
-            int fs = (int) (s + sp + b); // Сумма на счете, вместе с бонусами
-            System.out.println("Итоговая сумма на счете с бонусами " + fs);
+        int score = 1000; // Счет на телефоне
+        System.out.println("Счет на телефоне до пополнения " + score);
+        int deposite = 5000; // Cумма пополнения
+        System.out.println("Cумма пополнения " + deposite);
+        if (deposite >= 1000) {
+            float bonus = (float) (deposite * 0.01); // Сумма бонусов
+            System.out.println("Сумма бонусов при пополнении " + bonus);
+            int fullscore = (int) (score + deposite + bonus); // Сумма на счете, вместе с бонусами
+            System.out.println("Итоговая сумма на счете с бонусами " + fullscore);
         } else {
-            float b = (float) (sp * 0); // Сумма бонусов
-            System.out.println("Сумма бонусов при пополнении " + b);
-            int fs = (int) (s + sp + b); // Сумма на счете, вместе с бонусами
-            System.out.println("Итоговая сумма на счете с бонусами " + fs);
+            float bonus = (float) (deposite * 0); // Сумма бонусов
+            System.out.println("Сумма бонусов при пополнении " + bonus);
+            int fullscore = (int) (score + deposite + bonus); // Сумма на счете, вместе с бонусами
+            System.out.println("Итоговая сумма на счете с бонусами " + fullscore);
         }
 
 
